396384f009ac61804f23d5d7c0943a009b2ec4be
andreas hansson <andreas.hansson@icomera.com>
2018-08-22 13:15:58 +0200

    Added bg images

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import logo from '../images/toymachine-logo.png';
 
 const Menu = (props) => {
   return (
     <div className="menu-container">
-    <div><img src="test.jpg" /></div>
-    <ul>
-      <li className="link" onClick={props.changeview.bind(this, "home")}>Home</li>
-      <li className="link" onClick={props.changeview.bind(this, "booking")}>Book</li>
-      <li className="link" onClick={props.changeview.bind(this, "pictures")}>Pictures</li>
-      <li className="link" onClick={props.changeview.bind(this, "about_us")}>About us</li>
-      <li className="link" onClick={props.changeview.bind(this, "contact")}>Contact</li>
-    </ul>
+      <div className="logo-container"><img src={logo} /></div>
+      <ul>
+        <li className="link" onClick={props.changeview.bind(this, "home")}>Home</li>
+        <li className="link" onClick={props.changeview.bind(this, "booking")}>Book</li>
+        <li className="link" onClick={props.changeview.bind(this, "pictures")}>Pictures</li>
+        <li className="link" onClick={props.changeview.bind(this, "about_us")}>About us</li>
+        <li className="link" onClick={props.changeview.bind(this, "contact")}>Contact</li>
+      </ul>
     </div>
   )
 }
