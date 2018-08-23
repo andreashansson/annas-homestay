c572f445c6d02473ab97af63795ca522de120586
andreas hansson <andreas.hansson@icomera.com>
2018-08-23 09:12:58 +0200

    Underline in menu css styled

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from '../images/toymachine-logo.png';
+import '../css/Menu.css';
 
 const Menu = (props) => {
   return (
     <div className="menu-container">
       <div className="logo-container"><img src={logo} /></div>
       <ul>
-        <li className="link" onClick={props.changeview.bind(this, "home")}>Home</li>
-        <li className="link" onClick={props.changeview.bind(this, "booking")}>Book</li>
-        <li className="link" onClick={props.changeview.bind(this, "pictures")}>Pictures</li>
-        <li className="link" onClick={props.changeview.bind(this, "about_us")}>About us</li>
-        <li className="link" onClick={props.changeview.bind(this, "contact")}>Contact</li>
+        <li onClick={props.changeview.bind(this, "home")}><a href="#" className="link">Home</a></li>
+        <li onClick={props.changeview.bind(this, "booking")}><a href="#" className="link">Book</a></li>
+        <li onClick={props.changeview.bind(this, "pictures")}><a href="#" className="link">Pictures</a></li>
+        <li onClick={props.changeview.bind(this, "about_us")}><a href="#" className="link">About us</a></li>
+        <li onClick={props.changeview.bind(this, "contact")}><a href="#" className="link">Contact</a></li>
       </ul>
     </div>
   )
